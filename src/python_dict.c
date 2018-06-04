ce707450fec85d3196c4d7174be94a3e4e0fc04e
Hans Petter Jansson <hpj@cl.no>
2018-06-04 02:15:07 +0200

    Don't leak ints in Python driver.

--- src/python_dict.c
+++ src/python_dict.c
@@ -5,12 +5,14 @@ typedef PyObject * hash_t;
     hash_t hash = PyDict_New(); \
     PyObject * py_int_value = PyInt_FromLong(0);
 #define INSERT_INT_INTO_HASH(key, value) do { \
-        PyObject * py_int_key = PyInt_FromLong(key); /* leak */ \
+        PyObject * py_int_key = PyInt_FromLong(key); \
         PyDict_SetItem(hash, py_int_key, py_int_value); \
+        Py_DECREF (py_int_key); \
     } while(0)
 #define DELETE_INT_FROM_HASH(key) do { \
-        PyObject * py_int_key = PyInt_FromLong(key); /* leak */ \
+        PyObject * py_int_key = PyInt_FromLong(key); \
         PyDict_DelItem(hash, py_int_key); \
+        Py_DECREF (py_int_key); \
     } while(0)
 #define INSERT_STR_INTO_HASH(key, value) do { \
         PyDict_SetItemString(hash, key, py_int_value); \
