fd4e008af47e79e1c6301fd51e3530ed112ca3e1
Hans Petter Jansson <hpj@cl.no>
2018-06-06 21:26:35 +0200

    Let implementations manage their own string memory.

--- src/khash.c
+++ src/khash.c
@@ -7,13 +7,15 @@ KHASH_MAP_INIT_STR(khstr, void *)
   khash_t(kh64) *hash = kh_init(kh64); \
   khash_t(khstr) *str_hash = kh_init(khstr);
 
-#define INSERT_INT_INTO_HASH(key, value) kh_put (kh64, hash, key, &value)
+#define INSERT_INT_INTO_HASH(key, value) \
+  do { int r; khint_t k = kh_put (kh64, hash, key, &r); if (r > 0) { kh_value (hash, k) = &value; } } while (0)
 #define DELETE_INT_FROM_HASH(key) \
   do { khint_t k = kh_get (kh64, hash, key); \
-       kh_del (kh64, hash, k); } while (0)
-#define INSERT_STR_INTO_HASH(key, value) kh_put (khstr, str_hash, key, &value)
+       if (k != kh_end (hash)) kh_del (kh64, hash, k); } while (0)
+#define INSERT_STR_INTO_HASH(key, value) \
+  do { int r; char *istr = strdup (key); khint_t k = kh_put (khstr, str_hash, istr, &r); if (r > 0) { kh_value (str_hash, k) = &value; } else free (istr); } while (0)
 #define DELETE_STR_FROM_HASH(key) \
   do { khint_t k = kh_get (khstr, str_hash, key); \
-       kh_del (khstr, str_hash, k); } while (0)
+       if (k != kh_end (hash)) { char *istr = (char *) kh_key (str_hash, k); kh_del (khstr, str_hash, k); free (istr); } } while (0)
 
 #include "template.c"
--- src/qt_qhash.cc
+++ src/qt_qhash.cc
@@ -1,7 +1,7 @@
 #include <inttypes.h>
 #include <QHash>
 typedef QHash<int64_t, int64_t> hash_t;
-typedef QHash<const char *, int64_t> str_hash_t;
+typedef QHash<QString, int64_t> str_hash_t;
 #define SETUP hash_t hash; str_hash_t str_hash;
 #define INSERT_INT_INTO_HASH(key, value) hash.insert(key, value)
 #define DELETE_INT_FROM_HASH(key) hash.remove(key)
